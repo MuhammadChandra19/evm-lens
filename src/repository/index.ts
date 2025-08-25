e29170d51a722dedc798a1d3c732ad2dc2a3ed0a
Muhammad chandra zulfikar <muhammadchandrazulfikar@gmail.com>
2025-08-25 17:16:39 +0700

    feat: init repo

---
+++ src/repository/index.ts
@@ -0,0 +1,23 @@
+import { SQLocalDrizzle } from 'sqlocal/drizzle';
+import { drizzle } from 'drizzle-orm/sqlite-proxy';
+import playgroundRepository from './playground/query';
+import type { PlaygroundRepository } from './playground/query';
+import type { SnapshotRepository } from './snapshot/query'
+import snapshotRepository from './snapshot/query';
+
+type Repository = {
+  playground: PlaygroundRepository
+  snapshot: SnapshotRepository
+}
+
+const initRepository = (): Repository => {
+  const { driver, batchDriver } = new SQLocalDrizzle('evmdb.sqlite3');
+  const db = drizzle(driver, batchDriver);
+
+  return {
+    playground: playgroundRepository(db),
+    snapshot: snapshotRepository(db)
+  }
+}
+
+export default initRepository;
