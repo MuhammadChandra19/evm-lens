4976309684b28a60a1d2dfa80e939188c60581e0
Muhammad chandra zulfikar <muhammadchandrazulfikar@gmail.com>
2025-08-30 09:42:38 +0700

    feat: use prebuild token

--- src/pages/create-contract/forms/ContractPlaygroundSetup/data.ts
+++ src/pages/create-contract/forms/ContractPlaygroundSetup/data.ts
@@ -1,42 +1,19 @@
+import tokenBytecode from '@/service/evm-analyzer/script/token';
 import { ContractEVMSchema } from "./schema";
+import abi from "@/service/evm-analyzer/script/test_abi.json"
 
 const DEFAULT_DATA: ContractEVMSchema = {
   contractConfiguration: {
     contractAddress: "0x742d35Cc6ab8b2532c4b4b3d34d0f0d1f8b8c8d8",
     ownerAddress: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
     decimals: "18",
-    projectName: "",
+    projectName: "Test token",
     totalSupply: "1000000",
     initialOwnerBalance: "10",
   },
   bytecodeAndAbi: {
-    constructorBytecode:
-      "0x608060405234801561001057600080fd5b506040516107d03803806107d083398101604052810190508061002f576000803c8d6000fd5b50600160005560408051908101604052806020016040528060051b6040528060051b6040528160068201526004810191825260408051602001604052806020016040528060051b6040528060051b604052816006820",
-    contractAbi: `
-      [
-        {
-          "inputs": [
-            {"internalType": "uint256", "name": "_totalSupply", "type": "uint256"}
-          ],
-          "stateMutability": "nonpayable",
-          "type": "constructor"
-        },
-        {
-          "inputs": [],
-          "name": "totalSupply",
-          "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [{"internalType": "address", "name": "account", "type": "address"}],
-          "name": "balanceOf",
-          "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-          "stateMutability": "view",
-          "type": "function"
-        }
-      ]
-    `,
+    constructorBytecode: tokenBytecode,
+    contractAbi: JSON.stringify(abi),
   },
 };
 
