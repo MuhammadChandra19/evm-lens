d85487dc8670bd837ef28af6e7e13deed4d6e464
Muhammad chandra zulfikar <muhammadchandrazulfikar@gmail.com>
2025-08-30 10:14:00 +0700

    feat: move folders

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,8 +1,8 @@
-import Landing from "@/pages/landing";
-import CreateContract from "@/pages/create-contract";
-import Playground from "@/pages/playground";
-import PlaygroundProvider from "@/providers/Playground";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import Landing from '@/pages/landing';
+import CreateContract from '@/pages/create-contract';
+import Playground from '@/pages/playground/[id]';
+import PlaygroundProvider from '@/providers/Playground';
+import { createBrowserRouter, RouterProvider } from 'react-router';
 
 const RouterWrapper = () => {
   const router = createBrowserRouter([
